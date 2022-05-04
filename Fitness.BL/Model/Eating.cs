b7dfef8b0dc8ff522eaf7d2fc9f73deb2d271a04
Churkon Sergey <hotmerch@hotmail.com>
2022-05-04 23:21:00 +0300

    Add food in model

---
+++ Fitness.BL/Model/Eating.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fitness.BL.Model
+{
+    public class Eating
+    {
+        public DateTime Moment { get; }
+        public List<Food> Foods { get; }
+        public User User { get; }
+
+        public Eating(User user)
+        {
+            User = user ?? throw new ArgumentNullException(nameof(user));
+        }
+    }
+}
---
+++ Fitness.BL/Model/Food.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fitness.BL.Model
+{
+    public class Food
+    {
+        public string Name { get; }
+        public double Proteins { get; }
+        public double Fats { get; }
+        public double Carbohydrates { get; }
+        public double Callories { get; }
+
+        public Food(string name) : this(name, 0,0,0,0)
+        {
+            Name = name;
+        }
+
+        public Food(string name, double callories, double proteins, double fats, double carbohydates)
+        {
+            Name = name;
+            Callories = callories / 100f;
+            Proteins = proteins / 100f;
+            Carbohydrates = carbohydates / 100f;
+        }
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
